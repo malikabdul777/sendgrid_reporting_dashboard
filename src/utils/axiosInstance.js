f461bad78bcc30e80cb8b15744a66635ac501d88
Abdul Malik Shaik <malik7771@yahoo.com>
2024-10-19 08:46:02 -0400

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", // Replace with your base URL
+  baseURL: "https://sendgrid-reporting-dashboard-server.onrender.com",
 });
 
 export default axiosInstance;
