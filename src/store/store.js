9b579888fbc24c5c281bcc215f82cd8984c7745c
Abdul Malik Shaik <malik7771@yahoo.com>
2024-10-19 06:10:10 -0400

    Update store js

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./slices/userSlice";
 
 import { masterApiSlice } from "./apiSlices/masterApiSlice";
 import persistReducer from "redux-persist/es/persistReducer";
@@ -7,7 +6,7 @@ import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
-  user: userReducer,
+  // user: userReducer,
 });
 
 const persistConfig = {
